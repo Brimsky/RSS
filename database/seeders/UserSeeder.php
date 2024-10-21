7e6d4c793a00f640eadca800d4c1ddfd47aabaae
Brimsky <juruma66@gmail.com>
2024-10-21 18:18:03 +0300

    fixed categroy and subcategroy routing + creating listing fix

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        // Create 10 more random users
+        User::factory(10)->create();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\CheckoutController;
 use App\Http\Controllers\DeliveryController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
+// Public routes
 Route::get('/', function () {
     $search = request('search');
-
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
@@ -21,16 +21,13 @@ Route::get('/', function () {
     ]);
 })->name('home');
 
-// Public routes
 Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-// Route::get('/products/{category}', [ProductController::class, 'subcategories'])->name('products.subcategories');
-// Route::get('/products/{category}/{subcategory}', [ProductController::class, 'listings'])->name('products.listings');
-// Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
-
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show')->where('id', '[0-9]+');
+Route::get('/products/{category}', [ProductController::class, 'subcategories'])->name('products.subcategories');
+Route::get('/products/{category}/{subcategory}', [ProductController::class, 'listings'])->name('products.listings');
 
 Route::get('/delivery', [DeliveryController::class, 'index'])->name('delivery.index');
-Route::post('/delivery/store', [DeliveryController::class, 'store'])->name('delivery.store');
-
 Route::get('/api/categories', [ProductController::class, 'getCategories']);
 
 // Authentication required routes
@@ -41,7 +38,6 @@ Route::middleware(['auth'])->group(function () {
     Route::match(['patch', 'post'], '/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
     Route::post('/products', [ProductController::class, 'store'])->name('products.store');
     Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
     Route::put('/products/{product}', [ProductController::class, 'update'])->name('products.update');
@@ -54,6 +50,7 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/cart/remove', [CartController::class, 'remove'])->name('cart.remove');
 
     Route::post('/api/create-checkout-session', [CartController::class, 'createCheckoutSession']);
+    Route::post('/delivery/store', [DeliveryController::class, 'store'])->name('delivery.store');
 
     Route::get('/checkout/success', [CheckoutController::class, 'success'])->name('success');
     Route::get('/checkout/cancel', [CheckoutController::class, 'cancel'])->name('checkout.cancel');
