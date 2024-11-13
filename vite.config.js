c7d61671e0a68c7497c6995e6d665be12fd58cde
Brimsky <juruma66@gmail.com>
2024-11-13 17:05:47 +0200

    vite.config.js build fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app/**/*.vue', 'resources/js/components/**/*.vue'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
         }),
         vue(),
     ],
